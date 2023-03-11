47eb6b665e8426b8115112c8c5a93bc5eb9dcc77
Julicardeb <cardenasjulianacarolina@gmail.com>
2023-03-10 20:11:15 -0500

    Nombre

--- App/apps/navigation.py
+++ App/apps/navigation.py
@@ -1,7 +1,7 @@
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 from dash import html
-from app import app
+from App import app
 from dash.dependencies import Input, Output, State
 import dash
 
