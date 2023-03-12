a5a9982e8f90540df51d271b411566bf077d74df
Julicardeb <cardenasjulianacarolina@gmail.com>
2023-03-12 09:01:52 -0500

    Pagina de Inicio

--- TableroDash/pages/inicio.py
+++ TableroDash/pages/inicio.py
@@ -2,6 +2,7 @@ from dash import html
 from dash import dcc
 from dash.dependencies import Input, Output
 from apps import navigation
+import dash_bootstrap_components as dbc
 
 inicio_layout = html.Div(children=[
     navigation.navbar,
@@ -9,6 +10,23 @@ inicio_layout = html.Div(children=[
     html.H1(children = "Bienvenido al Programa de Predicción de Enfermedades Cardíacas",
             style={'textAlign': 'center'}),
     html.Br(),
+    html.Div(children='Está aplicación ayuda a predecir enfermedades cardíacas por medio de variables explicativas', style={
+        'textAlign': 'center'
+    }),
+    html.Br(),
+    html.Div(children=[
+        dbc.Button("Instrucciones", size="lg", id="inicio_instrucciones",href="/instrucciones",
+                   style={'margin-left': '620px',
+                            'margin-bottom': '10px',
+                            'verticalAlign': 'middle'})
+        ]),
+    html.Br(),
+    html.Div(children=[
+        dbc.Button("Programa", size="lg", id="inicio_programa",href="/programa",
+                   style={'margin-left': '635px',
+                            'margin-bottom': '10px',
+                            'verticalAlign': 'middle'})
+        ]),
     html.Div(children='Está aplicación ha sido creada por Santiago González y Juliana Cárdenas', style={
         'textAlign': 'center'
     }),
